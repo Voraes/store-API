534fe26abbaf002d687d6ab5d4c1cd59350734e8
victor <victor.inscriptio@gmail.com>
2023-02-27 11:38:00 -0300

    Adding query parameters and sorting

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 
 const { getAllProductsStatic, getAllProducts } = require('../controllers/products');
 
-router.route('/').get(getAllProducts());
-router.route('/static').get(getAllProductsStatic());
+router.route('/').get(getAllProducts);
+router.route('/static').get(getAllProductsStatic);
 
 module.exports = router;
